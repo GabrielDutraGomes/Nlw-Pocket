0d30d7c3a30235613403ceae9e46801485c70c70
Gabriel Dutra <bidielandrade@gmail.com>
2024-09-14 13:56:52 -0300

    .,

--- index.js
+++ index.js
@@ -1,33 +1,30 @@
-const { select, input } = require('@inquirer/prompts')
-const fs = require("fs").promisses
+const { select, input, checkbox } = require('@inquirer/prompts')
+const fs = require("fs").promises
 
-let mensagem = "Bem vindo!"
+let mensagem = "Bem vindo ao App de Metas!"
 
 let metas
 
-const carregarMetas = async() => {
-    try{
+const carregarMetas = async () => {
+    try {
         const dados = await fs.readFile("metas.json", "utf-8"),
         metas = JSON.parse(dados)
     }
-    catch (error)
+    catch (erro)
     {
         metas = []
     }
 }
 
-const salvarMetas = async() => {
+const salvarMetas = async () => {
     await fs.writeFile("metas.json", JSON.stringify(metas, null, 2))
 }
 
-const cadastrarMeta = async() => {
-    if(meta.length = 0) {
-        menssagem = "Nao existem metas!"
-        return
-    }
-    const meta = await input({message: "Digite a meta:"})
+const cadastrarMeta = async () => {
+    
+    const meta = await input({ message: "Digite a meta:" })
 
-    if(meta.length == 0) {
+    if (meta.length == 0) {
         mensagem = 'A meta nao pode ser vazia'
         return
     }
