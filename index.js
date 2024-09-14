29988dd615f3356a34819c20914f3635edb38ea6
Gabriel Dutra <bidielandrade@gmail.com>
2024-09-14 12:14:00 -0300

    ..

--- index.js
+++ index.js
@@ -59,10 +59,27 @@ const metasRealizadas = async () => {
     }
 
     await select({
-        message: "Metas Realizadas",
+        message: "Metas Realizadas" + realizadas.length,
         choices:[...realizadas]
     })
 }
+
+const metasAbertas = async () => {
+    const abertas = metas.filter((meta) => {
+        return !meta.checked
+    })
+
+    if(abertas.length == 0){
+        console.log('Nao existem metas abertas')
+        return
+    }
+
+    await select({
+        message: "Metas Abertas" + abertas.length,
+        choices:[...abertas]
+    })
+}
+
 
 const start = async () => {
 
@@ -84,6 +101,10 @@ const start = async () => {
                     value: "realizadas"
                 },
                 {
+                    name: "metas abertas",
+                    value: "abertas"
+                },
+                {
                     name: "Sair",
                     value: "sair"
                 }
@@ -100,6 +121,9 @@ const start = async () => {
                 break
             case "realizadas":
                 await metasRealizadas()
+                break
+            case "abertas":
+                await metasAbertas()
                 break
             case "sair":
                 console.log("Até a proxima...")
